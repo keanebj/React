314e380300468c2eac5d9ee23bf73ebcd16453f5
keanebj <wdf1822850036>
2018-03-28 23:07:48 +0800

    预览

--- react-native/AwesomeProject/App.js
+++ react-native/AwesomeProject/App.js
@@ -9,7 +9,14 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  TextInput,
+  ScrollView,
+  FlatList,
+  SectionList,
+  TouchableHighlight,
+  NavigatorIOS
 } from 'react-native';
 
 const instructions = Platform.select({
@@ -20,19 +27,190 @@ const instructions = Platform.select({
 });
 
 type Props = {};
-export default class App extends Component<Props> {
+
+import {
+  StackNavigator,
+} from 'react-navigation';
+
+const AppStackNavigator = StackNavigator({
+  Main: {screen: MainScreen},
+  Profile: {screen: ProfileScreen},
+});
+
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showText: true };
+
+    // 每1000毫秒对showText状态做一次取反操作
+    setInterval(() => {
+      this.setState(previousState => {
+        return { showText: !previousState.showText };
+      });
+    }, 1000);
+  }
+
+  render() {
+    // 根据当前showText的值决定是否显示text内容
+    let display = this.state.showText ? this.props.text : ' ';
+    return (
+      <Text>{display}</Text>
+    );
+  }
+}
+
+export default class NavigatorIOSApp extends Component {
   render() {
     return (
+      <NavigatorIOS
+        initialRoute={{
+          component: MyScene,
+          title: 'My Initial Scene',
+        }}
+        style={{flex: 1}}
+      />
+    )
+  }
+}
+
+class MyScene extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    navigator: PropTypes.object.isRequired,
+  }
+
+  constructor(props, context) {
+    super(props, context);
+    this._onForward = this._onForward.bind(this);
+  }
+
+  _onForward() {
+    this.props.navigator.push({
+      title: 'Scene ' + nextIndex,
+    });
+  }
+
+
+
+export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
+  getContent(){
+    return  fetch('https://facebook.github.io/react-native/movies.json')
+    .then((response) => {
+      // alert(JSON.stringify(response))
+      alert(response)
+      response.json()
+    })
+
+    .then((responseJson) => {
+      // alert(JSON.stringify(responseJson))
+      return responseJson.movies;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
+
+
+  render() {
+    let pic = {
+      uri: "https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg"
+    };
+
+   
+    return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+        <Text>导航</Text>
+
+        {/* <SectionList
+          sections={[
+            { title: 'D', data: ['Devin'] },
+            { title: 'E', data: ['Eevin','Eevin','Eevin','Eevin','Eevin','Eevin','Eevin','Eevin'] },
+            { title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie'] },
+          ]}
+          renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
+          renderSectionHeader={({ section }) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+        />
+
+        <FlatList data={[
+          { key: 'Devin' },
+          { key: 'Jackson' },
+          { key: 'James' },
+          { key: 'Joel' },
+          { key: 'John' },
+          { key: 'Jillian' },
+          { key: 'Jimmy' },
+          { key: 'Julie' },
+        ]}
+          renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}>
+
+        </FlatList> */}
+
+        <TouchableHighlight onPress={this.getContent}>
+          <Text>Button点击事件</Text>
+        </TouchableHighlight>
+        <Text style={styles.instructions} >
+          {instructions} 图片显示
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
+        <Image source={pic} style={styles.imgstyle}>
+
+        </Image>
+
+        {/* <Blink text='' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' /> */}
+
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({ text })}
+        />
+        <Text style={{ padding: 10, fontSize: 42 }}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
         </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+
+
+        <ScrollView>
+          <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Text style={{ fontSize: 96 }}>If you like</Text>
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Text style={{ fontSize: 96 }}>Scrolling down</Text>
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Text style={{ fontSize: 96 }}>What's the best</Text>
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Text style={{ fontSize: 96 }}>Framework around?</Text>
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Text style={{ fontSize: 80 }}>React Native</Text>
+
+        </ScrollView>
+
       </View>
     );
   }
@@ -41,9 +219,12 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 22,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    // flexDirection: 'column',
+    // justifyContent: 'space-between',
   },
   welcome: {
     fontSize: 20,
@@ -55,4 +236,23 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  imgstyle: {
+    width: 300,
+    height: 100,
+  },
+ 
+   sectionHeader: {
+     paddingTop: 2,
+     paddingLeft: 10,
+     paddingRight: 10,
+     paddingBottom: 2,
+     fontSize: 14,
+     fontWeight: 'bold',
+     backgroundColor: 'rgba(247,247,247,1.0)',
+   },
+   item: {
+     padding: 10,
+     fontSize: 18,
+     height: 44,
+   },
 });
